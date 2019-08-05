6ce32183cb6c01b6cefe78d7f95289a9e7bcba85
Mariselvan <mariselvan.murugasan@zoomrx.com>
2019-08-05 20:45:21 +0530

    Updates

---
+++ precache-manifest.eacde9acad0d0c81240eebd3238df0ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23fff5e8fed8f4f5f9a1e2220d879dd",
+    "url": "/mindsweeper/index.html"
+  },
+  {
+    "revision": "a188c1603a2b4fcacc5c",
+    "url": "/mindsweeper/static/css/main.835197e6.chunk.css"
+  },
+  {
+    "revision": "10c95e8605f18490ee42",
+    "url": "/mindsweeper/static/js/2.4a914921.chunk.js"
+  },
+  {
+    "revision": "a188c1603a2b4fcacc5c",
+    "url": "/mindsweeper/static/js/main.f71d8261.chunk.js"
+  },
+  {
+    "revision": "48770523de1acf007492",
+    "url": "/mindsweeper/static/js/runtime~main.5472b2c8.js"
+  },
+  {
+    "revision": "1e12ef9c62afb4c2bd793097ec62e011",
+    "url": "/mindsweeper/static/media/explosion.1e12ef9c.png"
+  }
+]);
